3a875ab5d21ca7e0abd810bcae88670c9dc08c86
Martin Palkov <martin.palkov@gmail.com>
2023-11-07 18:54:50 +0100

    progress backup

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,5 +1,15 @@
 const router = require("express").Router();
+const Celebrity = require("../models/Celebrity.model");
 
 // all your routes here
+
+router.get("/celebrities/create", (req, res) => {
+    res.render("celebrities/new-celebrity");
+});
+
+router.post("/celebrities/create", (req, res) => {
+    const data = req.body;
+    
+});
 
 module.exports = router;
