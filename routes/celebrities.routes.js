84af81afde2db87c65a1c4463f8e2e45235449ed
Martin Palkov <martin.palkov@gmail.com>
2023-11-08 23:12:40 +0100

    iterations 2, 3 and 4 done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -3,16 +3,37 @@ const Celebrity = require("../models/Celebrity.model");
 
 // all your routes here
 
-router.get("/celebrities/create", (req, res) => {
+router.get("/create", (req, res) => {
+	console.log("\tCELEB-CREATE GET");
     res.render("celebrities/new-celebrity");
 });
 
-router.post("/celebrities/create", async (req, res) => {
-    const data = req.body;
-    res.send(data);
-    //Celebrity.create()
-
-    
+router.post("/create", async (req, res) => {
+    try {
+		const data = req.body;
+		//res.send(data);
+		console.log("trying to create");
+		await Celebrity.create(data)
+		console.log("created");
+		res.render("celebrities");
+	}
+	catch {
+		console.error("ERROR");
+		res.render("celebrities/new-celebrity");
+	}
 });
+
+router.get("/", async (req, res, next) => {
+	try {
+		const allCelebsArray = await Celebrity.find();
+		res.render("celebrities/celebrities", {allCelebsArray});
+	}
+	catch{
+		console.log("ERROR");
+	}
+
+
+  });
+  
 
 module.exports = router;
